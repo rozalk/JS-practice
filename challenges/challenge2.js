1cfd0ca4c4a29c3a80310ad1051bebc54dd829f8
rozalk <rozalkarki9865@gmail.comm>
2023-12-22 23:36:26 +0545

    something error

--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -4,20 +4,22 @@ const value = Number(prompt("Enter a positive number?"));
 
 const isNum = !isNaN(value);
 // (value >= 65 && value <= 90) || (value >= 97 && value <= 122);
-if (!isNum) {
-  alert("Enter a number");
-} else {
-  if (value > 0) {
-    if (value % 3 === 0 && value % 7 === 0) {
-      alert("Number is multiple of 3 and 7");
+// if (!isNum) {
+//   alert("Enter a number");
+// } else {
+if (value > 0) {
+  if (value % 3 === 0 && value % 7 === 0) {
+    alert("Number is multiple of 3 and 7");
+  } else {
+    if (value % 3 === 0) {
+      alert("Number is multiple of 3");
+    } else if (value % 7 === 0) {
+      alert("Number is multiple of 7");
     } else {
-      if (value % 3 === 0) {
-        alert("Number is multiple of 3");
-      } else if (value % 7 === 0) {
-        alert("Number is neither multiple of 7");
-      } else {
-        alert("Enter a positive number:");
-      }
+      alert("Number is neither multiple of 3 and 7 ");
     }
   }
+} else {
+  alert("Enter a positive number");
 }
+// }
