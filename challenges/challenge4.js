3c414757f4aa5db3261e8e3d24915a89a1b23f0d
rozalk <rozalkarki9865@gmail.comm>
2023-12-23 19:31:36 +0545

    string or not and divided or not

--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -4,11 +4,22 @@
 const num1 = Number(prompt("Enter first number"));
 const num2 = Number(prompt("Enter second number:"));
 
-// Defining arrow function...
-const multiDivi = (num1, num2) => {
-  const multi = num1 * num2;
-  alert(`Multiplication is ${multi}`);
-  const divi = num1 / num2;
-  alert(`Division is ${divi}`);
-};
-const multiDivii = multiDivi(num1, num2);
+if (isNaN(num1) || isNaN(num2)) {
+  alert("Invalid Enter number");
+} else {
+  // Defining arrow function...
+  const multiDivi = (num1, num2) => {
+    const multi = num1 * num2;
+    alert(`Multiplication is ${multi}`);
+
+    if (num2 != 0) {
+      //check if it is divisible or not...
+      const divi = num1 / num2;
+      alert(`Division is ${divi}`);
+    } else {
+      alert("cannot be divided by zero");
+    }
+  };
+
+  const multiDivii = multiDivi(num1, num2); //function call...
+}
