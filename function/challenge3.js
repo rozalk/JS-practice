946ff8911b2f3df3db2a41b91e5267a4d38bb1ad
rozalk <rozalkarki9865@gmail.comm>
2023-12-25 23:17:59 +0545

    prime or not

--- function/challenge3.js
+++ function/challenge3.js
@@ -3,15 +3,16 @@ const number = Number(prompt("Enter any number"));
 
 if (number > 0) {
   const checkPrime = (number) => {
-    for (let i = 0; i < number; i++) {
-      if (number % i == 0) {
-        alert("not a prime");
-      } else {
-        alert("Prime number");
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        alert("Not a prime number");
+        return;
       }
     }
+    alert("Prime number");
   };
-  const check = checkPrime(number);
+
+  const result = checkPrime(number);
 } else {
   alert("Enter a positive number");
 }
