1b7c272baa4d4abff189ff9ff47babde17a60e91
rozalk <rozalkarki9865@gmail.comm>
2023-12-25 23:53:07 +0545

    compound interest

---
+++ function/challenge8.js
@@ -0,0 +1,17 @@
+//Write a function to calculate compound interest based on the principle amount
+const principal = Number(prompt("Enter principal amount"));
+
+const rate = Number(prompt("Enter rate of interest"));
+
+const time = Number(prompt("Enter time in year"));
+
+const no_interest = Number(
+  prompt("Enter number of times interest is compounded per year")
+);
+
+const compoundInterest = (p, t, r, n) => {
+  const amount = p * Math.pow(1 + r / n, n * t);
+  return amount;
+};
+const result = compoundInterest(principal, time, rate, no_interest);
+alert(`Compount Interest Amount is :${result}`);
